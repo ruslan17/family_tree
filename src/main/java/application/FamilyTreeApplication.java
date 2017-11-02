92e3bdbeaf8afcd8fdb3704627365b9f7f4eb254
r.tagiev <r.tagiev@riberry.az>
2017-11-02 16:31:32 +0400

    first commit

---
+++ src/main/java/application/FamilyTreeApplication.java
@@ -0,0 +1,19 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EntityScan(basePackages = {"application.model"})
+@EnableJpaRepositories(basePackages = {"application.dao"})
+public class FamilyTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyTreeApplication.class, args);
+    }
+
+}
